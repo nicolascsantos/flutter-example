aaec80b96adf8b6c8b1c26f0aeb9a2dd17ff6be3
Nicolas Carvalho <nicolas.carvalho@hotmail.com.br>
2022-08-14 00:08:20 -0300

    Adicionando mais containers e testando Row().

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,52 +1,59 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Primeiro app em Flutter.'),
-      ),
-      body: Column(
-        children: [
-          Container(
-            height: 771,
-            width: MediaQuery.of(context).size.width,
-            decoration: const BoxDecoration(
-              color: Colors.red,
-            ),
-            child: const Center(
-              child: Text(
-                'Olá mundo!',
-                style: TextStyle(color: Colors.white,
-                  fontSize: 30,
+        appBar: AppBar(
+          title: const Text('Primeiro app em Flutter.'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                color: Colors.red,
+              ),
+              child: const Center(
+                child: Text(
+                  'Olá mundo!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                  ),
                 ),
               ),
             ),
-          ),
-          Text('Blabla')
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Início'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Perfil'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Configurações'
-          ),
-        ],
-      )
-    );
+            Row(
+              children: [
+                Container(
+                  color: Colors.green,
+                  height: 100,
+                  width: 200,
+                  child: const Center(child: Text('Primeiro container')),
+                ),
+                Container(
+                  color: Colors.black,
+                  height: 100,
+                  width: 200,
+                  child: const Center(
+                      child: Text('Segundo container',
+                          style: TextStyle(color: Colors.white))),
+                )
+              ],
+            )
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Início'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Configurações'),
+          ],
+        ));
   }
 }
